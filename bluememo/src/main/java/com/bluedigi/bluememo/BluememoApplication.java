590cbef979e92abd5de4bed7f97ffb244e85d0e2
Emmanuel Martinez <edmvp98@gmail.com>
2026-06-21 20:35:31 -0600

    first commit

---
+++ bluememo/src/main/java/com/bluedigi/bluememo/BluememoApplication.java
@@ -0,0 +1,13 @@
+package com.bluedigi.bluememo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BluememoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BluememoApplication.class, args);
+	}
+
+}
---
+++ bluememo/src/test/java/com/bluedigi/bluememo/BluememoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bluedigi.bluememo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BluememoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
