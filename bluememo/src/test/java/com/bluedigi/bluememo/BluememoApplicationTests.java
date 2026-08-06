ba22c22dbb8fee35f5039f92be1b8b05fc901fc5
Emmanuel Martinez <edmvp98@gmail.com>
2026-08-06 02:18:09 -0600

    added profile local for test in mvn, added profiles

--- bluememo/src/test/java/com/bluedigi/bluememo/BluememoApplicationTests.java
+++ bluememo/src/test/java/com/bluedigi/bluememo/BluememoApplicationTests.java
@@ -2,8 +2,10 @@ package com.bluedigi.bluememo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BluememoApplicationTests {
 
 	@Test
